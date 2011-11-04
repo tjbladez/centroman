2de3b4ebedc044125a4338451ecdf3b2af44e373
Nickolay Schwarz <tjbladez@gmail.com>
2011-11-04 16:55:12 -0500

    Disabling selection single player

--- public/lib/game/entities/title.js
+++ public/lib/game/entities/title.js
@@ -7,23 +7,13 @@
       },
       animSheet: new ig.AnimationSheet('media/title.png', GameConfig.main.width, GameConfig.main.height),
       init: function(x, y, opts) {
-        this.addAnim("single", 0.5, [1, 0]);
-        this.addAnim("multi", 0.5, [3, 2]);
+        this.addAnim("idle", 0.5, [1, 0]);
         this.name = "title_screen";
-        this.currentAnim = this.anims['single'];
-        this.leftkey = opts.leftKey;
-        this.rightkey = opts.rightKey;
         return this.parent(x, y, opts);
       },
       update: function() {
         this.parent();
-        if (ig.input.pressed(this.leftKey)) {
-          this.currentAnim = this.anims['single'];
-        }
-        if (ig.input.pressed(this.rightKey)) {
-          this.currentAnim = this.anims['multi'];
-        }
-        if (ig.input.pressed('enter') && this.currentAnim === this.anims['multi']) {
+        if (ig.input.pressed('enter')) {
           this.kill();
           return ig.game.start();
         }
--- public/src/game/entities/title.coffee
+++ public/src/game/entities/title.coffee
@@ -9,18 +9,12 @@ ig.module(
     size: {x: GameConfig.main.width, y: GameConfig.main.height}
     animSheet: new ig.AnimationSheet('media/title.png', GameConfig.main.width, GameConfig.main.height)
     init: (x,y,opts) ->
-      this.addAnim("single", 0.5, [1, 0])
-      this.addAnim("multi", 0.5, [3, 2])
+      this.addAnim("idle", 0.5, [1, 0])
       @name        = "title_screen"
-      @currentAnim = @anims['single']
-      @leftkey     = opts.leftKey
-      @rightkey    = opts.rightKey
       this.parent(x, y, opts)
 
     update: ->
       this.parent()
-      @currentAnim = @anims['single'] if ig.input.pressed(@leftKey)
-      @currentAnim = @anims['multi']  if ig.input.pressed(@rightKey)
-      if ig.input.pressed('enter') && @currentAnim is @anims['multi']
+      if ig.input.pressed('enter')
         this.kill()
         ig.game.start()
